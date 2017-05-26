abc3c7698bcc75b1131075005c9edfc2203929a9
1530405 <1530405@045LAB04030>
2017-05-25 22:21:21 -0300

    Cadastro de Categorias; Lista de Categorias; Banco; Entity;

---
+++ Aula2505/Aula2505/Views/Categorias/CadastroCategoria.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CadastroCategoria.aspx.cs" Inherits="Aula2505.Views.Categorias.CadastroCategoria" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Cadastro de Categorias</h1>
+    <br />
+    
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+
+        <asp:Label ID="Nome" runat="server" Text="Nome"></asp:Label>
+        <asp:TextBox ID="txtNome" runat="server"></asp:TextBox>
+        <br /><br />
+
+        <asp:Label ID="Descrição" runat="server" Text="Descrição"></asp:Label>
+        <asp:TextBox ID="txtDescricao" runat="server"></asp:TextBox>
+        <br /><br />
+
+        <asp:Label ID="lblAtivo" runat="server" Text="Ativo"></asp:Label>
+        <asp:CheckBox ID="ckbAtivo" runat="server" />
+        <br /><br />
+
+        <asp:Button CssClass="btn btn-primary" ID="btnSalvar" runat="server" Text="Salvar" OnClick="btnSalvar_Click" />
+        <asp:Button CssClass="btn btn-danger" ID="btnCancelar" runat="server" Text="Cancelar" OnClick="btnCancelar_Click" />
+
+    </asp:UpdatePanel>
+
+</asp:Content>
---
+++ Aula2505/Aula2505/Views/Categorias/CadastroCategoria.aspx.cs
@@ -0,0 +1,43 @@
+﻿using Aula2505.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula2505.Views.Categorias
+{
+    public partial class CadastroCategoria : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = String.Empty;
+            txtDescricao.Text = String.Empty;
+            ckbAtivo.Checked = false;
+        }
+
+        protected void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Session["Nome"] = txtNome.Text;
+            Session["Descricao"] = txtDescricao.Text;
+            Session["Ativo"] = ckbAtivo.Checked;
+
+            BaseDadosContainer contexto = new BaseDadosContainer();
+
+            Categoria cat = new Categoria();
+
+            cat.Nome = Session["Nome"].ToString();
+            cat.Descricao = Session["Descricao"].ToString();
+            cat.Ativo = Convert.ToBoolean(Session["Ativo"].ToString());
+
+            contexto.Categorias.Add(cat);
+            contexto.SaveChanges();
+        }
+    }
+}
